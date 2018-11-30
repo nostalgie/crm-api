f478bbb19314b55ba7accfe5308bc85cd413f155
screamcha <scream1234540@gmail.com>
2018-11-30 17:20:26 +0300

    add error middleware

--- src/index.js
+++ src/index.js
@@ -6,11 +6,13 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const passport = require('passport')
 const router = require('./routes')
+const errorMiddleware = require('./utils/errorMiddleware')
 
 const app = express()
 app.use(cors())
 app.use(bodyParser.json())
 app.use(passport.initialize())
 app.use(router)
+app.use(errorMiddleware)
 
 app.listen(port)
